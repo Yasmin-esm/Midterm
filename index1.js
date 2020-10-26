06dcd226c6e4401e9994fda41978e64f5948888d
Yasmin-esm <37972569+Yasmin-esm@users.noreply.github.com>
2020-10-26 21:30:44 +0300

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,32 @@
+
+function changeNumbers()
+{
+
+  var num1 = document.getElementById("div1").innerHTML = Math.floor(Math.random() * 9) + 1;
+  var num2 = document.getElementById("div2").innerHTML = Math.floor(Math.random() * 9) + 1;
+  var num3 = document.getElementById("div3").innerHTML = Math.floor(Math.random() * 9) + 1;
+  var num4 = document.getElementById("div4").innerHTML = Math.floor(Math.random() * 9) + 1;
+  var num5 = document.getElementById("div5").innerHTML = Math.floor(Math.random() * 9) + 1;
+  var num6 = document.getElementById("div6").innerHTML = Math.floor(Math.random() * 9) + 1;
+  var num7 = document.getElementById("div7").innerHTML = Math.floor(Math.random() * 9) + 1;
+  var num8 = document.getElementById("div8").innerHTML = Math.floor(Math.random() * 9) + 1;
+  var num9 = document.getElementById("div9").innerHTML = Math.floor(Math.random() * 9) + 1;
+
+
+
+var colors = ["red", "orange", "yellow","green", "lightblue", "blue", "gray", "white", "pink"];
+
+
+var color1 = document.getElementById("div1").style.backgroundColor =colors[Math.floor(Math.random()*colors.length)];
+var color2 =document.getElementById("div2").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+var color3 =document.getElementById("div3").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+var color4 =document.getElementById("div4").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+var color5 =document.getElementById("div5").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+var color6 =document.getElementById("div6").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+var color7 =document.getElementById("div7").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+var color8 =document.getElementById("div8").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+var color9 =document.getElementById("div9").style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+
+
+
+}
